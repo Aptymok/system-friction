d056de2b442b461e937ff5655aa9820521505d04
Aptymok <115195341+Aptymok@users.noreply.github.com>
2026-07-04 05:36:06 -0600

    polish(root): mount institutional geometry override

--- src/components/root/SfiRootLiveConsole.tsx
+++ src/components/root/SfiRootLiveConsole.tsx
@@ -1,3 +1,15 @@
 'use client';
 
-export { default } from './RootConsoleScreen';
+import RootConsoleScreen from './RootConsoleScreen';
+import { RootConsoleInstitutionalOverride } from './RootConsoleInstitutionalOverride';
+
+type RootConsoleScreenProps = Parameters<typeof RootConsoleScreen>[0];
+
+export default function SfiRootLiveConsole(props: RootConsoleScreenProps) {
+  return (
+    <>
+      <RootConsoleInstitutionalOverride />
+      <RootConsoleScreen {...props} />
+    </>
+  );
+}
