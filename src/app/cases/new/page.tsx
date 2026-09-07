4c8a87d51dfd44cd4ef83a7f490a9af346ed02c3
Aptymok <115195341+Aptymok@users.noreply.github.com>
2026-09-07 13:37:33 -0600

    feat(cases): add human NEW CASE route

---
+++ src/app/cases/new/page.tsx
@@ -0,0 +1,7 @@
+import { NewCaseIngress } from '@/components/sfi/NewCaseIngress';
+
+export const dynamic = 'force-dynamic';
+
+export default function NewCasePage() {
+  return <NewCaseIngress />;
+}
